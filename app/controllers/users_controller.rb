b3543f412de42855066eec7bb881740dfb3cc617
Nick-Damico <nickalan82@icloud.com>
2018-11-25 15:48:26 -0500

    Fix rendering errors for json requests to :create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      flash[:alert] = "Successfully Sign Up"
-      redirect_to user_url(@user)
-    else
-      render :new, status: :bad_request
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html { redirect_to @user, alert: 'Successfully Signed Up' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
