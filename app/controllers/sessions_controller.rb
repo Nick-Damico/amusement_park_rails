7d6b4069611d06d4a59957903063f0f204dff60e
Nick-Damico <nickalan82@icloud.com>
2018-11-26 11:26:15 -0500

    update render shared/alert to shared/flash_messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,32 @@
 class SessionsController < ApplicationController
+  # before_action :get_user, only: [:create]
+  include SessionsHelper
+
   def new
   end
 
   def create
+    user = User.find_by(name: params[:user][:name])
+    respond_to do |format|
+      if user && user.authenticate(params[:user][:password])
+        # login
+        session[:user_id] = user.id
+        format.html {}
+        format.json {}
+      else
+        # error code
+        format.html {
+          flash.now[:danger] = "Invalid Name/Password match"
+          render :new
+        }
+        format.json {}
+      end
+    end
   end
 
   def destroy
   end
+
+  private
+
 end
