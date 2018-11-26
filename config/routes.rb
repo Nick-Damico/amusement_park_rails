a8a7158690fae049ac97bd26686f220911379c1b
Nick-Damico <nickalan82@icloud.com>
2018-11-25 20:37:28 -0500

    Add routes for 'get' /login, 'post' /login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'new/create'
+  get 'new/destroy'
   root 'static#home'
   resources :users
 
   get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
 end
