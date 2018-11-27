332413c99b63db09418a385a8c41c3bbdae94e72
Nick-Damico <nickalan82@icloud.com>
2018-11-27 10:10:03 -0500

    Define application_controller #logged_in which redirects to home with :alert 'Access Denied...' if an un authenticated user tries to access users/:id paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def current_user    
+  def logged_in
+      unless session[:user_id]
+        flash.now[:alert] = "Access Denied. Must login to View User profiles"
+        redirect_to root_url
+      end
   end
+
 end
