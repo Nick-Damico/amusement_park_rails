32ec60026f638cc40a4da3823064f08fb1dc8352
Nick-Damico <nickalan82@icloud.com>
2018-11-27 12:16:01 -0500

    #is_admin? moved to private method scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,28 +4,29 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
   # Validations
-    validates :name, presence: true,
-                     uniqueness: true
-    validates :height, presence: true,
-                       numericality: { only_integer: false }, unless: :is_admin?
-    validates :happiness, presence: true,
-                          numericality: { only_integer: true,
-                                          greater_than: 0,
-                                          less_than: 6,
-                                        }, unless: :is_admin?
-    validates :nausea, presence: true,
-                       numericality: { only_integer: true,
-                                       greater_than: 0,
-                                       less_than: 6
-                                     }, unless: :is_admin?
-    validates :tickets, presence: true,
-                        numericality: { only_integer: true }, unless: :is_admin?
-
-  def is_admin?
-    self.admin
-  end
+  validates :name, presence: true,
+                   uniqueness: true
+  validates :height, presence: true,
+                     numericality: { only_integer: false }, unless: :is_admin?
+  validates :happiness, presence: true,
+                        numericality: { only_integer: true,
+                                        greater_than: 0,
+                                        less_than: 6,
+                                      }, unless: :is_admin?
+  validates :nausea, presence: true,
+                     numericality: { only_integer: true,
+                                     greater_than: 0,
+                                     less_than: 6
+                                   }, unless: :is_admin?
+  validates :tickets, presence: true,
+                      numericality: { only_integer: true }, unless: :is_admin?
 
   def mood
     happiness > nausea ? 'happy' : 'sad'
+  end
+
+  private
+  def is_admin?
+    admin
   end
 end
