dd5793b343fa490be51f652c9a568ad8a1a72745
Nick-Damico <nickalan82@icloud.com>
2018-11-27 10:12:38 -0500

    Update before_action :logged_in? to renamed method :logged_in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :get_user, only: [:show]
-  before_action :logged_in?, only: [:show]
+  before_action :logged_in, only: [:show]
 
   def new
     @user = User.new
