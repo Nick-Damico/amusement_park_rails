835de8eb0d5493f564234bf4f0de0039094f1c93
Nick-Damico <nickalan82@icloud.com>
2018-11-28 09:39:23 -0500

    Update :show action, Add ride post action

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -2,4 +2,26 @@ class AttractionsController < ApplicationController
   def index
     @attractions = Attraction.all
   end
+
+  def show
+    @attraction = Attraction.find_by(id: params[:id])
+    respond_to do |format|
+      if @attraction
+        format.html {
+          render :show
+        }
+        format.json {}
+      else
+        format.html {
+          flash[:error] = "Ride #{params[:id]} doesn't exist"
+          redirect_to attractions_url
+        }
+        format.json {}
+      end
+    end
+  end
+
+  def ride
+
+  end
 end
