26535ef15485cfaa4b2ab0906e51b9ca3fcfa5c2
Nick-Damico <nickalan82@icloud.com>
2018-11-28 10:36:17 -0500

    Define #take_ride that updates a users attributes with attractions modifying data

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,4 +1,12 @@
 class Attraction < ApplicationRecord
   has_many :rides
   has_many :users, through: :rides
+
+  def take_ride(user)
+    user.update_attributes(
+      tickets: user.tickets -= tickets,
+      nausea: user.nausea += nausea_rating,
+      happiness: user.happiness += happiness_rating,
+    )
+  end
 end
